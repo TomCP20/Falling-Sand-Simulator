14d7566318351be842df8b06307ab8c22e049e84
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-08-30 13:43:33 +0100

    refactored window settings

--- Program.cs
+++ Program.cs
@@ -3,15 +3,9 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
-const int screenWidth = 640;
-const int screenHeight = 480;
-
 NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
 {
-    ClientSize = new Vector2i(screenWidth, screenHeight),
-
     Title = "Falling Sand Simulator",
-    // This is needed to run on macos
     Flags = ContextFlags.ForwardCompatible,
     WindowState = WindowState.Fullscreen,
 };
